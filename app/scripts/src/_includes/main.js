dba205a644b990363772a28a5073bb0111c73a76
Evgen <evgen904@gmail.com>
2020-09-23 15:42:51 +0400

    fix

--- app/scripts/src/_includes/main.js
+++ app/scripts/src/_includes/main.js
@@ -1,6 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function() {
 
-    document.querySelector('.js-btn-share-link').addEventListener('click', function (){
+    if (window.innerHeight >= 1000) {
+        document.querySelector('.js-booked-object').classList.add('active')
+    }
+
+    // Поделиться ссылкой на оплату
+    document.querySelector('.js-btn-share-link').addEventListener('click', () => {
         if (!document.querySelector('.js-share-link--modal').classList.contains('active')) {
             document.querySelector('.js-share-link--modal').classList.add('active')
         } else {
@@ -9,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     })
 
+    // Объект бронирования
     document.querySelector('.js-booked-object-toggle').addEventListener('click', function (){
         if (!document.querySelector('.js-booked-object').classList.contains('active')) {
             document.querySelector('.js-booked-object').classList.add('active')
@@ -17,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     })
 
+    // Промокод
     document.querySelector('.js-payment-coupon-toggle').addEventListener('click', function (){
         if (!document.querySelector('.js-payment-coupon').classList.contains('active')) {
             document.querySelector('.js-payment-coupon').classList.add('active')
@@ -25,16 +32,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     })
 
-    document.querySelector('.js-copy-url').addEventListener('click', function (){
-        const copyText = document.createElement("input")
-        copyText.classList.add('copy-input')
-        copyText.value = window.location.href
-        document.body.appendChild(copyText)
-        copyText.select()
-        document.execCommand("copy")
-        this.innerText = 'Ссылка скопирована'
+    // Скопировать ссылку
+    document.querySelectorAll('.js-copy-url').forEach(div => {
+        div.addEventListener('click', () => {
+            const copyText = document.createElement("input")
+            copyText.classList.add('copy-input')
+            copyText.value = window.location.href
+            document.body.appendChild(copyText)
+            copyText.select()
+            document.execCommand("copy")
+            div.innerText = 'Ссылка скопирована'
+        });
     })
 
+    // Отказаться от платежа, вызов модального окна
+    document.querySelector('.js-refuse-pay').addEventListener('click', function (){
+        document.querySelector('.js-refuse-pay-modal').classList.add('show')
+    })
+
+    // Отказаться от платежа, закрытие модального окна
+    document.querySelectorAll('.js-refuse-pay-close-modal').forEach(div => {
+        div.addEventListener('click', () => {
+            document.querySelector('.js-refuse-pay-modal').classList.remove('show')
+        });
+    })
+
+    document.querySelector('.js-show-payment-methods').addEventListener('click', function (){
+        if (!document.querySelector('.js-other-payment').classList.contains('show')) {
+            document.querySelector('.js-other-payment').classList.add('show')
+        } else {
+            document.querySelector('.js-other-payment').classList.remove('show')
+
+            let selectPayment = document.querySelector('input[name=payment_id]:checked').value
+            if (selectPayment !== '1' && selectPayment !== '2' && selectPayment !== '3') {
+                document.getElementById("payment_method_1").checked = true;
+            }
+        }
+    })
 
 
 
